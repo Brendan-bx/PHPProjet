c2ccd83fccff42c68e0f18032dc6e5ef02dcb1d6
Alexandre <alexbx2002@gmail.com>
2021-02-10 11:50:48 +0100

    Catégories

--- nourriture.php
+++ nourriture.php
@@ -1,9 +1,56 @@
 <?php
 require_once("header.php")
 ?>
-
 <a href ="categories.php">Back</a>
+
+
+<div class="card">
+  <img src="/img/Burger.png" alt="Burger" style="width:100%">
+  <h1>Burger simple</h1>
+  <p class="price">$15.99</p>
+  <p>Burger pour ceux qui ont la flemme</p>
+  <p><button>Add to Cart</button></p>
+</div>
+
+<div class="card">
+  <img src="/img/Frites.png" alt="Frites" style="width:100%">
+  <h1>Frites</h1>
+  <p class="price">$19.99</p>
+  <p>Pour rajouter un supplément au burger ;)</p>
+  <p><button>Add to Cart</button></p>
+</div>
 
 <?php
 require_once("footer.php")
 ?>
+
+<style type='text/css'>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  max-width: 300px;
+  margin: auto;
+  text-align: center;
+  font-family: arial;
+}
+
+.price {
+  color: grey;
+  font-size: 22px;
+}
+
+.card button {
+  border: none;
+  outline: 0;
+  padding: 12px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+  font-size: 18px;
+}
+
+.card button:hover {
+  opacity: 0.7;
+}
+</style>
--- telephone.php
+++ telephone.php
@@ -1,9 +1,54 @@
 <?php
 require_once("header.php")
 ?>
-
 <a href ="categories.php">Back</a>
 
+<div class="card">
+  <img src="/img/Samsung.png" alt="Samsung" style="width:100%">
+  <h1>Samsung Galaxy</h1>
+  <p class="price">$499.99</p>
+  <p>Les androids c'est mieux</p>
+  <p><button>Add to Cart</button></p>
+</div>
+
+<div class="card">
+  <img src="/img/Iphone12.png" alt="Iphone" style="width:100%">
+  <h1>Iphone 12</h1>
+  <p class="price">$699.99</p>
+  <p>Le même téléphone tout les ans</p>
+  <p><button>Add to Cart</button></p>
+</div>
+
+<style type='text/css'>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  max-width: 300px;
+  margin: auto;
+  text-align: center;
+  font-family: arial;
+}
+
+.price {
+  color: grey;
+  font-size: 22px;
+}
+
+.card button {
+  border: none;
+  outline: 0;
+  padding: 12px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+  font-size: 18px;
+}
+
+.card button:hover {
+  opacity: 0.7;
+}
+</style>
 
 <?php
 require_once("footer.php")
